9726391fbf1b083c96957578381e4c0c7858f1a1
Praarthanaa Jaisankar <p.jaisankar@stud.uni-goettingen.de>
2025-01-26 02:21:00 +0100

    Create seed_data.sql
    Basic sql seed data

---
+++ database/seed_data.sql
@@ -0,0 +1,3 @@
+# Sample data
+INSERT INTO foldseek_results (query_id, target_id, pident, alnlen, mismatch, gapopen, qstart, qend, tstart, tend, evalue, bitscore, tlen)
+VALUES ('1TIM_A', '1BTM_B', 95.6, 120, 1, 2, 1, 120, 1, 120, 0.0, 200.5, 150);
